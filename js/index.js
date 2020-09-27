d2ac12fa4cd0815bb3c46c2583dac6f600d40295
axin <1962892647@qq.com>
2020-09-27 19:04:54 +0800

    第一次提交

---
+++ js/index.js
@@ -0,0 +1,33 @@
+$(function () {
+    $('.item').on('click', 'a', function () {
+        var page = $(this).text();
+        console.log(page);
+        getNewsList(page);
+    });
+    window.onbeforeunload = function (e) {
+        console.log(111);
+        getNewsList(page);
+    }
+    function getNewsList(page) {
+        $.ajax({
+            url: 'https://photo.sina.cn/aj/index?page=' + page + '&cate=recommend',
+            // 代表我们要发起JSONP的数据请求
+            dataType: 'jsonp',
+            jsonp: 'callback',
+            jsonpCallback: '__onGetData__',
+            success: function (res) {
+                console.log(res)
+                var htmlStr = template('tpl-news', res)
+                $('.content').html(htmlStr)
+            }
+        })
+        $(document).ajaxStart(function () {
+            $(".loading_gif").css("display", "block")
+        })
+        $(document).ajaxStop(function () {
+            $(".loading_gif").css("display", "none")
+        })
+    }
+    getNewsList();
+
+})
